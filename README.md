5acb21f82ff24a9665988606ace0515e6c56630d
Aivars Liepa <aiciz@inbox.lv>
2017-12-13 23:48:14 +0200

    sendgrid webhook

---
+++ README.md
@@ -0,0 +1,4 @@
+# react-app
+Personal project for learning MERN stack
+
+Deployed version at: https://still-anchorage-97049.herokuapp.com/
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const Path = require("path-parser");
+const { URL } = require("url");
 const mangoose = require("mongoose");
 const Survey = mangoose.model("surveys");
 const requireLogin = require("../middlewares/requireLogin");
@@ -39,4 +42,19 @@ module.exports = app => {
       }
     }
   );
+
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const path = new Path("/api/surveys/:surveyId/:choice");
+    const uniqueClickEventData = _.chain(req.body)
+      .filter(({ event }) => event === "click")
+      .map(({ url, email }) => {
+        const match = path.test(new URL(url).pathname);
+        if (match) return { email, ...match };
+      })
+      .compact()
+      .uniqBy(e => e.email + e.surveyId)
+      .value();
+    console.log(uniqueClickEventData);
+    res.send({});
+  });
 };
